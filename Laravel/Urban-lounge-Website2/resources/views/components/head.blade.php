8dafd0ddc1e9e8d7cd5d7c38585f2b0719c6bfa1
thays bell <ps221122@summacollege.com>
2022-07-30 18:26:55 +0200

    start

---
+++ Laravel/Urban-lounge-Website2/resources/views/components/head.blade.php
@@ -0,0 +1,7 @@
+<head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet"/>
+        <meta http-equiv="refresh" content="10"/>
+        <script src="https://cdn.tailwindcss.com"></script>
+</head>
---
+++ Laravel/Urban-lounge-Website2/resources/views/layout/app.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('components.head')
+
+<body>
+    @include('components.nav')
+</body>
+
+</html>
